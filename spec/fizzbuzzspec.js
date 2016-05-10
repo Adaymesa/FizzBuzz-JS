87d1f2bad49dd29564e47775e26be0836d9e4c8e
Jack Riminton <jriminton@gmail.com>
2016-05-10 15:15:40 +0100

    Fizzbuzz complete

--- spec/fizzbuzzspec.js
+++ spec/fizzbuzzspec.js
@@ -13,10 +13,27 @@ describe("Fizzbuzz", function() {
 		});
 	});
 
-		describe("Know when a number is not", function(){
+	describe("Know when a number is not", function(){
 
-			it("is divisible by 3", function(){
-				expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
-			});
+		it("is divisible by 3", function(){
+			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
 		});
+	});
+
+	describe("when playing, says", function() {
+
+		it('"Fizz" when a number is divisible by 3', function() {
+			expect(fizzbuzz.says(3)).toEqual("Fizz");
+		});
+
+		it('"Buzz" when a number is divisible by 5', function() {
+			expect(fizzbuzz.says(5)).toEqual("Buzz");
+		});
+
+		it('"Fizzbuzz" when a number is divisible by 3 and 5', function() {
+			expect(fizzbuzz.says(15)).toEqual("Fizzbuzz");
+		});
+	});
+
+		
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,31 @@
 var FizzBuzz = function(){};
-FizzBuzz.prototype.isDivisibleByThree = function(number) {
-	return (number % 3 === 0);
+
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+	return (number % divisor === 0);
 };
+
+FizzBuzz.prototype.isDivisibleByThree = function(number) {
+	return this._isDivisibleBy(number, 3);
+};
+
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+	return this._isDivisibleBy(number, 5);
+};
+
+FizzBuzz.prototype.isDivisibleByThreeAndFive = function(number) {
+	return this._isDivisibleBy(number, 15);
+};
+
+FizzBuzz.prototype.says = function(number) {
+	if (this.isDivisibleByThreeAndFive(number)) {
+		return "Fizzbuzz";
+	}
+	if (this.isDivisibleByThree(number)) {
+		return "Fizz";
+	}
+	if (this.isDivisibleByFive(number)) {
+		return "Buzz";
+	}
+	return number;
+};
+
